f06ecb9403d36b4513a345841fd7b636bfd530da
Lewis Peckover <lew@lew.io>
2018-05-15 01:11:22 +0000

    consul needs script args in >1

--- consul/consul.go
+++ consul/consul.go
@@ -79,9 +79,9 @@ func (r *ConsulAdapter) buildCheck(service *bridge.Service) *consulapi.AgentServ
 			check.Timeout = timeout
 		}
 	} else if cmd := service.Attrs["check_cmd"]; cmd != "" {
-		check.Script = fmt.Sprintf("check-cmd %s %s %s", service.Origin.ContainerID[:12], service.Origin.ExposedPort, cmd)
+		check.Args = [3]string{"/bin/sh", "-c", fmt.Sprintf("check-cmd %s %s %s", service.Origin.ContainerID[:12], service.Origin.ExposedPort, cmd))
 	} else if script := service.Attrs["check_script"]; script != "" {
-		check.Script = r.interpolateService(script, service)
+		check.Args = [3]string{"/bin/sh", "-c", r.interpolateService(script, service))
 	} else if ttl := service.Attrs["check_ttl"]; ttl != "" {
 		check.TTL = ttl
 	} else if tcp := service.Attrs["check_tcp"]; tcp != "" {
